f943d42f79d437205b86650673df641200cb7013
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2023-09-16 22:45:57 +0100

    Poll for updated tags

--- app/routes/upload/upload-land-boundary.js
+++ app/routes/upload/upload-land-boundary.js
@@ -37,11 +37,24 @@ module.exports = [{
       stream.push(null)
       const blobClient = await uploadInboundFile(stream, filename)
 
-      for (let i = 0; i < 5; i++) {
-        const tags = await blobClient.getTags()
-        console.log('TAGS:')
-        console.log(tags)
-        await new Promise(resolve => setTimeout(resolve, 1000))
+      const maxTime = 10000
+      const interval = 100
+      let scanDone = false
+      let timeTaken = 0
+      let tags
+
+      do {
+        tags = await blobClient.getTags()
+        scanDone = tags.blobTagSet.length !== 0
+        await new Promise(resolve => setTimeout(resolve, interval))
+        timeTaken += interval
+      } while (!scanDone && (timeTaken < maxTime))
+
+      if (scanDone) {
+        console.log(`Scan results ready in ${timeTaken / 1000} seconds`)
+        console.log(`Scan result: ${tags.blobTagSet[0].value}`)
+      } else {
+        console.log(`Scan results not ready after ${maxTime / 1000} seconds`)
       }
 
       return h.view('upload/upload-land-boundary')
