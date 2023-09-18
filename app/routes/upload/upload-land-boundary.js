0ec3e00d566b47fcb484136b43418a61b5485dc9
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2023-09-18 13:06:40 +0100

    Poll blog tags (#1)
    * Poll blog tags
    
    * Poll for updated tags
    
    * bump version
    
    ---------
    
    Co-authored-by: steve-dickinson <steve.dickinson@defra.gov.uk>

--- app/routes/upload/upload-land-boundary.js
+++ app/routes/upload/upload-land-boundary.js
@@ -35,7 +35,28 @@ module.exports = [{
       const stream = new Readable()
       stream.push(fileBuffer)
       stream.push(null)
-      await uploadInboundFile(stream, filename)
+      const blobClient = await uploadInboundFile(stream, filename)
+
+      const maxTime = 10000
+      const interval = 100
+      let scanDone = false
+      let timeTaken = 0
+      let tags
+
+      do {
+        tags = await blobClient.getTags()
+        scanDone = tags.blobTagSet.length !== 0
+        await new Promise(resolve => setTimeout(resolve, interval))
+        timeTaken += interval
+      } while (!scanDone && (timeTaken < maxTime))
+
+      if (scanDone) {
+        console.log(`Scan results ready in ${timeTaken / 1000} seconds`)
+        console.log(`Scan result: ${tags.blobTagSet[0].value}`)
+      } else {
+        console.log(`Scan results not ready after ${maxTime / 1000} seconds`)
+      }
+
       return h.view('upload/upload-land-boundary')
     }
   }
